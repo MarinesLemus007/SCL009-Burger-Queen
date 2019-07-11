7a79591d363d284641cae35c12944e38dcacb146
Cloiw <sircli@hotmail.com>
2019-07-11 17:42:15 -0400

    json de menu y pruebas

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import Btn from './Btn'
+import Menu from './data/menu.json'
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          Pruebita
+        </p>
+      {Menu.Desayunos.map(nombre=><Btn name={nombre.name} value={nombre.value}/>)}
+      
+      </header>
+
+    </div>
+  );
+}
+
+export default App;
