2d5879be46cc53d16c0602cb2d00527382f9d12b
MarinesLemus007 <marineslemus@gmail.com>
2019-07-12 14:13:16 -0400

    Haciendo ejercicios con el react

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import logo from './logo.svg';
+//import './App.css';
+
+const App = () =>(
+
+<div>
+<h1>Hola Mundo desde mi archivo en App.js</h1>
+<p> Saludo desde mi componente</p>
+<img src="https://www.somosmamas.com.ar/wp-content/uploads/2017/11/emoji-feliz.png" />
+</div>
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> Hola mundo.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
+
+)
+
+export default App;
