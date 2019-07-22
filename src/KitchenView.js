2f117fdfae03323c937f7b8a1aac0fc31f4553b9
MarinesLemus007 <marineslemus@gmail.com>
2019-07-22 18:20:16 -0400

    Trabajando en la vista de la cocina

--- src/KitchenView.js
+++ src/KitchenView.js
@@ -1,16 +1,43 @@
 import React from 'react';
 import Navbar from './navbar';
+import {db} from './data/firebase';
 
 class KitchenView extends React.Component{
-    // constructor(props) {
-    //     super(props);
-    //   }
+    constructor(props) {
+        super(props);
+        this.state = {}
+       
+      }
+
+
+    componentDidMount() {
+        db.collection("ordenes").get().then(querySnapshot => {
+           this.setState({
+                data: querySnapshot.docs.map(doc =>{
+                   
+                    return {data: doc.data()}
+                })
+            })
+               
+        });
+          
+    }
+
     render(){
+        const {data} = this.state;
+     
         return (
             <>
             <Navbar/>
+       
         <div>
-            
+        {data && data !== undefined ? data.map(el=>
+        <div>
+            {el.data.list.map(e=>
+            <p>{e.name} {e.value}</p>)}
+        <p>cliente : {el.data.client}</p>
+        <p>su pedido es </p>
+        </div>) : <p>error</p>}
            "oli"
         </div>
     </>)
