be2493698a0820a51fbc7b666862eb5d3cfaf4eb
Cloiw <sircli@hotmail.com>
2019-07-26 12:10:05 -0400

    arreglando tiempo de entrega en historial

--- src/components/OrderRecordTemplate.js
+++ src/components/OrderRecordTemplate.js
@@ -11,7 +11,7 @@ class OrderRecordTemplate extends React.Component{
                 <p>{"Fecha :  "+new Date(el.data.time).toLocaleDateString()}</p>
                 <p>{"Hora de pedido : "+new Date(el.data.time).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})}</p>
                 <p>{"Tiempo en cocina : "+new Date(el.data.time_ready - el.data.time).getMinutes()+" minutos" }</p>
-                <p>{"Entregado a las : "+new Date(el.data.time_ready).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})}</p>
+                <p>{"Entregado a las : "+new Date(el.data.time_delivered).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})}</p>
                 <p>{"Cliente : "+el.data.client}</p>
                 {el.data.list.map(e=> 
                  <p key={e.name+e.id}>{e.count+"  x  "+e.name}</p> )}
